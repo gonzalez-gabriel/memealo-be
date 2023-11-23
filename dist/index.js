c2d6c41510c6f9cd51652aa3b5fc9db2f1373ea5
TobiasGonzalezz <tobygon988@gmail.com>
2023-11-23 19:40:27 -0300

    Remove dist files

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// src/index.ts
-console.log("Hello World");
-function sum(a, b) {
-  return a + b;
-}
-console.log(sum(1, 8));
-//# sourceMappingURL=index.js.map
--- dist/index.mjs
+++
@@ -1,7 +0,0 @@
-// src/index.ts
-console.log("Hello World");
-function sum(a, b) {
-  return a + b;
-}
-console.log(sum(1, 2));
-//# sourceMappingURL=index.mjs.map
