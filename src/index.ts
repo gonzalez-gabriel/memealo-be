d3439e29b7d49386e1f03c047b1215e55053a728
Matias Nicolas Quintana <matiasnicolasquintana09@gmail.com>
2023-11-27 19:39:09 -0300

    implementation socket from backend

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express from 'express'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+import type { Request, Response } from 'express'
+
+const PORT = process.env.PORT ?? 3000
+
+const app = express()
+
+const server = createServer(app)
+
+const io = new Server(server, {
+  connectionStateRecovery: {
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log('user has connected!')
+
+  socket.on('disconnected', () => {
+    console.log('an user has disconnected')
+  })
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  })
+})
+
+app.get(['/', '/login'], (_req: Request, res: Response) => {
+  res.sendFile(process.cwd() + '/client/login.html')
+})
+
+app.get('/chat', (_req: Request, res: Response) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+})
+
+server.listen(3000, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
