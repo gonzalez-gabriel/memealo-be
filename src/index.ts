945bd09be9334d00f7c8e809dbb6458062ff25fe
Matias Nicolas Quintana <matiasnicolasquintana09@gmail.com>
2023-11-27 21:33:11 -0300

    develop modified

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express'
 
 const app = express()
@@ -6,5 +5,4 @@ const app = express()
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
 })
-=======
->>>>>>> 18dc1b42682303513fdea878079ee71774801d9c
+
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,10 @@
-import { defineConfig } from "tsup"
+import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
   sourcemap: true,
-  clean: true,
-<<<<<<< HEAD
+  clean: true
 })
-=======
-})
->>>>>>> 18dc1b42682303513fdea878079ee71774801d9c
