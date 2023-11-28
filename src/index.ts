8e97da8a031c92cf78cbabbb75681ab5896556a1
Matias Nicolas Quintana <matiasnicolasquintana09@gmail.com>
2023-11-28 10:27:00 -0300

    merge branch develop in implementation sockets

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 
+const PORT = process.env.PORT
+
 const app = express()
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+  console.log(`Server is running on port ${PORT}`)
 })
-
