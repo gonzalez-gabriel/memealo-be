18dc1b42682303513fdea878079ee71774801d9c
Matias Nicolas Quintana <matiasnicolasquintana09@gmail.com>
2023-11-27 21:19:08 -0300

    Merge branch 'develop' of https://github.com/gonzalez-gabriel/memealo-be into prueba

---
+++ src/routes/user.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+import { getAllUsers, createUser, getUser, updateUser } from '@/controllers/user'
+
+const router = Router()
+
+router.route('/')
+  .get(getAllUsers)
+  .post(createUser)
+
+router.route('/:id')
+  .get(getUser)
+  .put(updateUser)
+export { router }
