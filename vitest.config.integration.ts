820cf9c036babf27e56234b5865e1b1df6a064b4
TobiasGonzalezz <tobygon988@gmail.com>
2023-11-28 19:52:58 -0300

    Update dependencies and test file paths

--- vitest.config.integration.ts
+++ vitest.config.integration.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['src/tests/**/*.test.ts'],
+    include: ['src/tests/integration/**/*.test.ts'],
     threads: false,
     setupFiles: ['src/tests/helpers/setup.ts']
   },
