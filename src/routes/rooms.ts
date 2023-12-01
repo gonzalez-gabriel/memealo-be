b5cedaec57c8e4ab075d3ba7f30ff7ac73fc6362
facu <facundo19burgos@gmail.com>
2023-11-30 21:20:01 -0300

    update user  and rooms

--- src/routes/rooms.ts
+++ src/routes/rooms.ts
@@ -1,15 +1,14 @@
 import { Router } from 'express'
 import { getAllRooms, createRoom, getRoom, updateRoom } from '@/controllers/room.controller'
 
-const router = Router()
+const roomsRouter = Router()
 
-router.route('/')
+roomsRouter.route('/rooms')
   .get(getAllRooms)
   .post(createRoom)
 
-router.route('/:id')
-
+roomsRouter.route('/rooms/:id')
   .get(getRoom)
   .put(updateRoom)
 
-export { router }
+export { roomsRouter }
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express'
 import { getAllUsers, createUser, getUser, updateUser } from '@/controllers/user'
 
-const router = Router()
+const userRouter = Router()
 
-router.route('/')
+userRouter.route('/users')
   .get(getAllUsers)
   .post(createUser)
 
-router.route('/:id')
+userRouter.route('/users/:id')
   .get(getUser)
   .put(updateUser)
-export { router }
+  
+export { userRouter }
