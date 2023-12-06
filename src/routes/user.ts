d324cdbc287297d20b1b8b5422d95b40c361eef2
Sebastian Celsi <sebastian.celsi@gmail.com>
2023-12-05 21:06:23 -0300

    modified routes/user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express'
 import { getAllUsers, createUser, getUser, updateUser } from '@/controllers/user'
+import { bodyUserSchema, idUserSchema, updateUserSchema } from '@/schemas/user'
+import { schemaValidator } from '@/middlewares/schemaValidator'
 
 const userRouter = Router()
 
 userRouter.route('/users')
   .get(getAllUsers)
-  .post(createUser)
+  .post(schemaValidator(bodyUserSchema),createUser)
 
 userRouter.route('/users/:id')
-  .get(getUser)
-  .put(updateUser)
+  .get(schemaValidator(idUserSchema), getUser)
+  .put(schemaValidator(updateUserSchema), updateUser)
   
 export { userRouter }
